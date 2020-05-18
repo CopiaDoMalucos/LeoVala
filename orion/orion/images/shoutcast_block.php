92361761af1d252a0bdb8ad5907c988170b4a84d
CopiaDoMalucos <64922627+CopiaDoMalucos@users.noreply.github.com>
2020-05-18 14:08:13 -0300

    111

--- orion/orion/images/shoutcast_block.php
+++
@@ -1,8 +0,0 @@
-<?    
-if (!$site_config["MEMBERSONLY"] || $CURUSER) {
-begin_block("P2BS Live Feed");
-$showradioinfo="on";
-include("radio-info.php");
-end_block();
-}
-?>
--- orion/orion/snat/snat.php
+++
@@ -1,4 +0,0 @@
-<?
-echo"test";
-print("test");
-?>
